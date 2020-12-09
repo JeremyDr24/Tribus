b6e10fc0016d606222c10860a744cd3094d3c331
JeremyDr24 <jeremy.durrieu@gmail.com>
2020-12-09 10:59:54 +0100

    Mise à jour de la stratégie

--- tribus.py
+++ tribus.py
@@ -1,6 +1,7 @@
 import random
 import math
 
+
 class Tribe:
     name = ""
 
@@ -10,6 +11,7 @@ class Tribe:
     def hunt(self, sesChoix):
         return 0
 
+
 class BetrayTribe(Tribe):
 
     def __init__(self):
@@ -18,14 +20,15 @@ class BetrayTribe(Tribe):
     def hunt(self, sesChoix):
         return 1
 
+
 class CoopTribe(Tribe):
 
     def __init__(self):
         self.name = "coopTribe"
 
     def hunt(self, sesChoix):
-
         return 0
+
 
 class RndTribe(Tribe):
 
@@ -33,7 +36,8 @@ class RndTribe(Tribe):
         self.name = "rndTribe"
 
     def hunt(self, sesChoix):
-        return random.randint(0,2)
+        return random.randint(0, 2)
+
 
 class MyTribe(Tribe):
 
@@ -41,67 +45,83 @@ class MyTribe(Tribe):
         self.name = "Mana"
 
     def hunt(self, sesChoix):
-        #Détecter le début de partie pour pouvoir commencer par de la coop
+
+        roundActuel = 1
+        moy = 0
+        vingtaineTab = list()
+
+        nbTrahison = 0
+        nbCoop = 0
+        nbRenon = 0
+
+        global phaseTrahison
+        phaseTrahison = []
+        ret = 0
+
+        # Détecter le début de partie pour pouvoir commencer par de la coop
         print(sesChoix)
         if sesChoix[0] == -1:
             return 0
 
-        #Chaque 10 tours, vérifier si la moyenne est supérieure à 1.5. Sinon, renoncer
-
-        #Obtenir le numéro de manche actuelle
-        roundActuel= 0
-        nbDizaine = 0
-        moy = 0
-        dizaineTab = list()
-        nbTrahison = 0
-        global phaseTrahison
+        # Obtenir le numéro de manche actuelle
         for i in sesChoix:
             if sesChoix[i] != -1:
                 roundActuel += 1
                 moy += sesChoix[i]
-                dizaineTab.append(sesChoix[i])
+                vingtaineTab.append(sesChoix[i])
 
                 print("\nRound:", roundActuel)
                 print("Valeur:", i)
 
-                #Vérifier toutes les 10 manches la moyenne des 10 manches précédentes
+                # Calculer toutes les 20 manches la moyenne
                 if roundActuel % 20 == 0:
-                    nbDizaine += 1
-                    moy /= 10
+                    moy /= 20
                     print("Moyenne:", moy)
-                    print("Tableau:", dizaineTab)
+                    print("Tableau:", vingtaineTab)
 
-                    #Si la moyenne est inférieure à 1.5, on renonce
+                    # Si la moyenne est inférieure à 1.5, on renonce
                     if moy <= 1.5:
-                        return 2
+                        ret = 2
 
-                    #Renoncer si il a renoncé le tour précédent
+                    # Renoncer si il a renoncé le tour précédent
                     if sesChoix[i] == 2:
-                        return 2
+                        ret = 2
 
-                    #Récupérer le nombre de trahison sur les 10 dernières manches
-                    for j in range(0, len(dizaineTab), 1):
-                        if dizaineTab[j] == 1:
+                    # Récupérer le nombre de coopérations sur les 20 dernières manches
+                    for j in range(0, len(vingtaineTab), 1):
+                        if vingtaineTab[j] == 0:
+                            nbCoop += 1
+                            if nbCoop >= 3:
+                                ret = 1
+                                nbCoop = 0
+
+
+                    # Récupérer le nombre de trahisons sur les 20 dernières manches
+                    for j in range(0, len(vingtaineTab), 1):
+                        if vingtaineTab[j] == 1:
                             nbTrahison += 1
-                            return 2
 
-                    dizaineTab.clear()
+                    # Récupérer le nombre de coopérations sur les 20 dernières manches
+                    for j in range(0, len(vingtaineTab), 1):
+                        if vingtaineTab[j] == 2:
+                            nbRenon += 1
+
+
+                    vingtaineTab.clear()
+                    moy = 0
+
+        return ret
+
                     #TODO:
                     # - Si il renonce, renoncer aussi
                     # - Récupérer le nombre de trahison (sur les 10 dernières? Nb total?)
                     # - Si j'ai été trahis le tour précédent, trahir en retour
 
-
-                    moy = 0
-
-        return 1
-
-
 def match(t1, t2, nbRounds):
-    r1 = 0#récompenses tribu1
-    r2 = 0#récompense tribu2
+    r1 = 0  # récompenses tribu1
+    r2 = 0  # récompense tribu2
 
-    #historique
+    # historique
     t1_choices = list()
     t2_choices = list()
 
@@ -109,21 +129,21 @@ def match(t1, t2, nbRounds):
         t1_choices.append(-1)
         t2_choices.append(-1)
 
-    #match
+    # match
     for round in range(0, nbRounds):
-        #faire les choix
+        # faire les choix
         choice1 = t1.hunt(t2_choices)
         choice2 = t2.hunt(t1_choices)
 
-        #maj historique
+        # maj historique
         t1_choices[round] = choice1
         t2_choices[round] = choice2
 
-        #ajout recompense
+        # ajout recompense
         if choice1 == 2:
             if choice2 == 2:
-                r1+=2
-                r2+=2
+                r1 += 2
+                r2 += 2
             elif choice2 == 0:
                 r1 += 2
                 r2 += 1
@@ -132,8 +152,8 @@ def match(t1, t2, nbRounds):
                 r2 += 1
         elif choice1 == 0:
             if choice2 == 2:
-                r1+=1
-                r2+=2
+                r1 += 1
+                r2 += 2
             elif choice2 == 0:
                 r1 += 4
                 r2 += 4
@@ -142,8 +162,8 @@ def match(t1, t2, nbRounds):
                 r2 += 6
         elif choice1 == 1:
             if choice2 == 2:
-                r1+=1
-                r2+=2
+                r1 += 1
+                r2 += 2
             elif choice2 == 0:
                 r1 += 6
                 r2 += 1
@@ -152,6 +172,7 @@ def match(t1, t2, nbRounds):
                 r2 += 1
 
     return (r1, r2)
+
 
 def tournoi(tribes):
     nbRounds = 100
@@ -167,18 +188,18 @@ def tournoi(tribes):
             tribe2 = tribes[j]
             if i < j:
                 (score1, score2) = match(tribe1, tribe2, nbRounds)
-                print(tribe1.name + " vs "+ tribe2.name)
-                print(str(score1)+" - "+str(score2))
+                print(tribe1.name + " vs " + tribe2.name)
+                print(str(score1) + " - " + str(score2))
                 points[i] += score1
                 points[j] += score2
                 if score1 > score2:
-                    wins[i]+=1
+                    wins[i] += 1
                 elif score1 < score2:
-                    wins[j]+=1
+                    wins[j] += 1
 
-    print("Final result")
+    print("\nFinal result")
     for i in range(0, len(tribes)):
-        print(tribes[i].name+" "+str(points[i])+" "+str(wins[i]))
+        print(tribes[i].name + " " + str(points[i]) + " " + str(wins[i]))
 
 
 tribes = list()
